94670506bd1565179b9f529fb4d2ad88352e9fe5
Florencia Miranda <flo.mirandai@gmail.com>
2019-03-21 00:07:41 +0100

    se accede a la cuestion seleccionada

--- js/profesor.js
+++ js/profesor.js
@@ -3,8 +3,6 @@ function cargar_cuestiones() {
 
   var cuestiones = datos.cuestiones;
   var main_cuestiones = document.getElementById("cuestiones");
-  var cuestion_nueva = document.getElementById("nueva_cuestion");
-  console.log(typeof cuestion_nueva);
   for (let cuestion of cuestiones) {
     var nueva_cuestion = crear_cuestion(cuestion);
     main_cuestiones.appendChild(nueva_cuestion);
@@ -14,15 +12,17 @@ function cargar_cuestiones() {
 function crear_cuestion(cuestion) {
   var card_div = document.createElement("div");
   card_div.className = "card";
-  card_div.id = cuestion.clave;
 
   var card_body = document.createElement("div");
   card_body.className = "card-body";
+
   var link_cuestion = document.createElement("a");
   link_cuestion.href = "pagina_cuestion.html"; //TODO: cambiar este link!
   var mensaje = document.createTextNode(cuestion.enunciado);
   link_cuestion.appendChild(mensaje);
   link_cuestion.onclick = abrir_cuestion;
+  link_cuestion.id = cuestion.clave;
+
   var span_eliminar = document.createElement("span");
   span_eliminar.className = "delete-btn";
   var boton_eliminar = document.createElement("a");
@@ -38,12 +38,31 @@ function crear_cuestion(cuestion) {
 
   return card_div;
 }
+
+//abrir_cuestion: setea la cuestion que se seleccionó para poder cargarla en la otra pagina.
 function abrir_cuestion() {
-  //agregar al local storage la cuestion que estoy viendo ahora y cargar la nueva pagina.
-  // al carga la nueva pagina en onload lo que se hara es sacar la info de la cuestion y ahi mostrar la info necesaria.
-  console.log("abrir cuestion!");
+  var datos = JSON.parse(window.localStorage.getItem("datos"));
+  var cuestiones = datos.cuestiones;
+  for (let cuestion of cuestiones) {
+    if (cuestion.clave == this.id) {
+      window.localStorage.setItem("cuestion_actual", JSON.stringify(cuestion));
+      return;
+    }
+  }
 }
 
+//cargar_cuestion: funcion que hace que se carguen los datos de la cuestion que hemos apretado.
 function cargar_cuestion() {
-  console.log("se abrio la pagina de la cuestion!");
+  var cuestion_actual = JSON.parse(
+    window.localStorage.getItem("cuestion_actual")
+  );
+  var div_nombre = document.getElementById("nombre_cuestion");
+  console.log(cuestion_actual.enunciado);
+  var input_nombre = document.createElement("input");
+  input_nombre.type = "text";
+  input_nombre.className = "form-control";
+  input_nombre.value = cuestion_actual.enunciado;
+  input_nombre.required = true;
+
+  div_nombre.insertBefore(input_nombre, div_nombre.childNodes[2]);
 }
