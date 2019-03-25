312495d6b9e9ebc8b886a4b8e0a728b8c0b6f580
Florencia Miranda <flo.mirandai@gmail.com>
2019-03-25 16:12:14 +0100

    Se edita el nombre de la cuestion

--- js/inicio_cuestion_m.js
+++ js/inicio_cuestion_m.js
@@ -9,6 +9,48 @@ function cargar_cuestion() {
   input_nombre.className = "form-control";
   input_nombre.value = cuestion_actual.enunciado;
   input_nombre.required = true;
+  input_nombre.id = "input_nombre";
 
   div_nombre.insertBefore(input_nombre, div_nombre.childNodes[2]);
+}
+
+//funcion que toma el nuevo enunciado, comprueba que se haya modificado.
+//Si se modificó el enunciado, lo setea en los datos de localstorage y en cuestion_actual.
+function cambio_enunciado() {
+  var cuestion_actual = JSON.parse(
+    window.localStorage.getItem("cuestion_actual")
+  );
+  var nuevo_enunciado = window.document.getElementById("input_nombre").value;
+  if (nuevo_enunciado != cuestion_actual.enunciado) {
+    var datos = JSON.parse(window.localStorage.getItem("datos"));
+    var cuestiones = datos.cuestiones;
+    cuestiones = cambiar_cuestion(
+      cuestiones,
+      cuestion_actual.clave,
+      nuevo_enunciado
+    );
+    datos.cuestiones = cuestiones;
+    console.log("datos", datos);
+    window.localStorage.setItem("datos", JSON.stringify(datos));
+    cuestion_actual.enunciado = nuevo_enunciado;
+    console.log("cuestion_actual", cuestion_actual);
+    window.localStorage.setItem(
+      "cuestion_actual",
+      JSON.stringify(cuestion_actual)
+    );
+  }
+  console.log("nuevo enunciado", cuestion_actual.enunciado);
+}
+
+/*Funcion que busca la cuestion a cambiar y le cambia la clave.
+ Genera un nuevo arreglo de cuestiones con las mismas que habian antes y con la que se modificó */
+function cambiar_cuestion(cuestiones, id, nuevo_enunciado) {
+  var nuevas_cuestiones = [];
+  for (let cuestion of cuestiones) {
+    if (cuestion.clave == id) {
+      cuestion.enunciado = nuevo_enunciado;
+    }
+    nuevas_cuestiones.push(cuestion);
+  }
+  return nuevas_cuestiones;
 }
